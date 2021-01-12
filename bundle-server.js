36420521c9d304145e9c58a28e6c7dd5771cac7d
ariwald <arisouza@gmail.com>
2021-01-12 14:50:35 +0100

    chat pics

--- bundle-server.js
+++ bundle-server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(require("./build.js"));
 
 app.listen(8081, () =>
-    console.log(`8081 listens Ready to compile and serve bundle.js`)
+  console.log(`8081 listens Ready to compile and serve bundle.js`)
 );
